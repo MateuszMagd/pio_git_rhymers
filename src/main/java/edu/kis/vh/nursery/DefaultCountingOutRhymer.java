4f26239b63f9b21c1e247cda88a146b4ccf34299
Hidennnn <240629@edu.p.lodz.pl>
2023-04-14 13:30:24 +0200

    8. Dodanie stałych, finali oraz Override'ów. Poprawa konwencji nazewniczej klasy RhymersFactory

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -2,9 +2,14 @@ package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
 
-    private int[] numbers = new int[12];
+    public static final int NUMBERS_SIZE = 12;
+    public static final int TOTAL_STARTING_VALUE = -1;
+    public static final int EMPTY_TABLE = -1;
+    public static final int FULL_TABLE = 11;
 
-    public int total = -1;
+    final private int[] numbers = new int[NUMBERS_SIZE];
+
+    public int total = TOTAL_STARTING_VALUE;
 
     public void countIn(int in) {
         if (!isFull())
@@ -12,11 +17,11 @@ public class DefaultCountingOutRhymer {
     }
 
     public boolean callCheck() {
-        return total == -1;
+        return total == EMPTY_TABLE;
     }
 
     public boolean isFull() {
-        return total == 11;
+        return total == FULL_TABLE;
     }
 
     protected int peekaboo() {
