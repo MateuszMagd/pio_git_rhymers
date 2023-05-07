9a96db872f151f3ee115ccbbd898127f149083eb
MateuszMagd <mateusz.mag7@gmail.com>
2023-05-07 18:01:30 +0200

    12. Usuniecie setterow

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -9,28 +9,8 @@ public class DefaultCountingOutRhymer {
     private static final int RETURN_WITH_EMPTY_TABLE = -1;
     private final int[] numbers = new int[NUMBERS_TABLE_SIZE];
 
-    public static int getNumbersTableSize() {
-        return NUMBERS_TABLE_SIZE;
-    }
-
-    public static int getTotalStartValue() {
-        return TOTAL_START_VALUE;
-    }
-
-    public static int getEmptyTable() {
-        return EMPTY_TABLE;
-    }
-
-    public static int getFullTable() {
-        return FULL_TABLE;
-    }
-
     public int getTotal() {
         return total;
-    }
-
-    public void setTotal(int total) {
-        this.total = total;
     }
 
     private int total = TOTAL_START_VALUE;
--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -2,7 +2,7 @@ package edu.kis.vh.nursery;
 
 public class HanoiRhymer extends DefaultCountingOutRhymer {
 
-    public static final int TOTAL_REJECTED_START_VALUE = 0;
+    private static final int TOTAL_REJECTED_START_VALUE = 0;
     private int totalRejected = TOTAL_REJECTED_START_VALUE;
 
     public int reportRejected() {
